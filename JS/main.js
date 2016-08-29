13170a36f20a188e09c7c1fb2f7d858ad3373eb0
Michael Fenwick <mickyfen17@aol.com>
2016-08-28 17:01:11 -0700

    further experimenting with JQuery & CSS3

--- JS/main.js
+++ JS/main.js
@@ -12,15 +12,53 @@ $(document).ready(function() {
 			$("#main-header").fadeIn(1000);
 		}
 	});
+
 	//Increases/decreses size of logos on mouse enter/leave
 	$(".logo").mouseenter(function() {
 		$(this).stop().animate({
 			width: "165px"
-		});
+		}, function() {
+			$(".logo").mouseleave(function() {
+				$(this).stop().animate({
+					width: "150px"
+				}); 
+	  		}); //mouseleave
+		}); //callback
+	}); //mouseenter
+
+
+	// $(window).scroll(function() {
+	// 	if($(window).scrollTop() > 150) {
+	// 		$("#Home").addClass("border");
+	// 	} else if($(window).scrollTop() > 549) {
+	// 		$("#Home").removeClass("border");
+	// 	} else {
+	// 		$("#Home").removeClass("border");
+	// 	}
+	// });
+	// $(window).scroll(function() {
+	// 	if($(window).scrollTop() > 550) {
+	// 		$("#About").addClass("border");
+	// 	} else {
+	// 		$("#About").removeClass("border");
+	// 	}
+	// });
+	// $(window).scroll(function() {
+	// 	if($(window).scrollTop() > 850) {
+	// 		$("#Services").addClass("border");
+	// 	} else {
+	// 		$("#Services").removeClass("border");
+	// 	}
+	// });
+
+	$(window).scroll(function() {
+		if($(window).scrollTop() > 150) {
+			$("#Bord1").animate({width: "+=100%"}, 2000);
+		} if($(window).scrollTop() > 550) {
+			$("#Bord2").animate({width: "+=100%"}, 2000);
+		} if($(window).scrollTop() > 850) {
+			$("#Bord3").animate({width: "+=100%"}, 2000);
+		}
 	});
-	$(".logo").mouseleave(function() {
-		$(this).stop().animate({
-			width: "150px"
-		});
-	});
+
 });
